871fde51df502542a539fba3621f76928315e414
Rubén Céspedes Heredia <rubenn569@gmail.com>
2018-03-11 02:28:32 +0100

    Proyecto inicial
    Realizada portada del museo. Se reutilizara la cabecera, el lateral auxiliar y el pie.

---
+++ header.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Modern Museum</title>
+        <link href="css/app.css" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+        <div class="header-wrapper medium-width">
+    		<a href="index.php"><div class="logo-container">
+    			<img src="assets/image/logo.png">
+    		</div></a>
+            <div class="title-container">
+                <h1>Modern Museum</h1>
+            </div>
+    		<div class="links-container">
+    			<ul>
+    				<li><a class= "no-select" href="">Menú 1</a></li>
+    				<li><a class= "no-select" href="">Menú 2</a></li>
+    				<li><a class= "no-select" href="">Menú 3</a></li>
+    			</ul>
+    		</div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,73 @@
+<?php include 'header.php';?>
+
+    <section class="index-page page-content no-margin-top">
+
+        <aside class="menu small-width">
+
+            <p>Sidebar de prueba. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Mendrerit id, lorem. Maecenas nec odio e sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
+        </aside>
+        <div class="categorias small-width">
+            <p class="title">Últimas ofertas</p>
+            <div class="categorias-cards-container">
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>1</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>2</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>3</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>4</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>5</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>6</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>7</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>8</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+                <div class="offer-card-container">
+                    <div class="photo-container" style="background-image: url('assets/image/museo-prueba.jpg');">
+                        <div class="discount"><p>9</p></div>
+                    </div>
+                    <div class="title">Prueba | <span class="smaller-txt">Íncreible</span></div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+<?php include 'footer.php';?>
+
+        
