f6b4e44b3922f73e028786fa289c867e2e92e080
Rubén Céspedes Heredia <rubenn569@gmail.com>
2018-04-09 16:24:54 +0200

    Fichero JavaScript reubicado en carpeta js

---
+++ js/myScript.js
@@ -0,0 +1,76 @@
+var prohibidas = ["hola", "adios", "tu", "yo", "nosotros"];
+
+function desplegarComentarios(){
+	document.getElementById("contenedor").style.display = "block";
+}
+
+function ocultarComentarios(){
+	document.getElementById("contenedor").style.display = "none";
+}
+
+function deteccion(){
+	var num_palabras = prohibidas.length;
+	var i;
+	var encontrado = -1;
+	var texto = document.getElementById("comentario").value;
+	var nuevo_texto;
+
+	for(i=0; i < num_palabras && encontrado === -1; i=i+1){
+		encontrado = texto.indexOf(prohibidas[i]);
+	}
+
+	if(encontrado !== -1){
+		var asteriscos = "*";
+		var k;
+
+		for (k=0; k < prohibidas[i-1].length-1; k=k+1) {
+            asteriscos += "*";
+        }
+
+		nuevo_texto = texto.replace(prohibidas[i-1],asteriscos);
+
+		document.getElementById("comentario").value = nuevo_texto;
+	}
+}
+
+function aniadir_comentario(){
+	var nombre, d, fecha, hora, comentario;
+
+    nombre = document.getElementById("nombre").value;
+    nombre += " ";
+    nombre += document.getElementById("apellidos").value;
+    d = new Date();
+    fecha = d.getDate();
+    fecha += "/";
+
+    if(d.getMonth()+1 < 10){
+    	fecha += "0";
+    }
+
+    fecha += d.getMonth()+1;
+    fecha += "/";
+    fecha += d.getFullYear();
+    hora = d.getHours();
+    hora += ":";
+    
+    if(d.getMinutes() < 10){
+    	hora += "0";
+    }
+    
+    hora += d.getMinutes();
+    comentario = document.getElementById("comentario").value;
+
+    var html = document.getElementById("contenedor").innerHTML;
+    var indice = html.indexOf("<form");
+
+    var parte1 = html.slice(0,indice);
+    var parte2 = html.slice(indice,html.length);
+
+    document.getElementById("contenedor").innerHTML = parte1 + 
+    "<div class='comentario'><div class='superior'><div class='autor'><p>AUTOR: " + nombre + 
+    "</p></div><div class='contenedor-fecha'><div class='fecha'><p>FECHA: " + fecha + 
+    "</p></div><div class='hora'><p>HORA: " + hora + 
+    "</p></div></div></div><div class='inferior'><p>" + comentario + 
+    "</p></div></div>" +
+    parte2;
+}
