7eae58852e36d43aff673ffdb2bcf617f57c32d9
Rubén Céspedes Heredia <rubenn569@gmail.com>
2018-05-20 01:37:21 +0200

    Aniadido boton y formulario para loguearse
    Sin funcionalidad. Solo estetica

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 	function imprimirHeader(){
 		echo "<header>
-	        <div class='header-wrapper medium-width'>
+	        <div class='header-wrapper medium-width inline'>
 	    		<a href=\"index.php\"><div class='logo-container'>
 	    			<img src=\"./assets/image/logo.png\">
 	    		</div></a>
@@ -16,6 +16,39 @@
 	    			</ul>
 	    		</div>
 	        </div>
+	        <!-- Button to open the modal login form -->
+			<button class=\"loginbtn\" onclick=\"document.getElementById('id01').style.display='block'\">Login</button>
+
+			<!-- The Modal -->
+			<div id=\"id01\" class=\"modal\">
+			  <span onclick=\"document.getElementById('id01').style.display='none'\" 
+			class=\"close\" title=\"Cerrar login\">&times;</span>
+
+			  <!-- Modal Content -->
+			  <form class=\"modal-content animate\" action=\"/action_page.php\">
+			    <div class=\"imgcontainer\">
+			      <img src=\"/images/img_avatar2.png\" alt=\"Avatar\" class=\"avatar\">
+			    </div>
+
+			    <div class=\"container\">
+			      <label for=\"uname\"><b>Username</b></label>
+			      <input type=\"text\" placeholder=\"Enter Username\" name=\"uname\" required>
+
+			      <label for=\"psw\"><b>Password</b></label>
+			      <input type=\"password\" placeholder=\"Enter Password\" name=\"psw\" required>
+
+			      <button type=\"submit\">Login</button>
+			      <label>
+			        <input type=\"checkbox\" checked=\"checked\" name=\"remember\"> Remember me
+			      </label>
+			    </div>
+
+			    <div class=\"container\" style=\"background-color:#f1f1f1\">
+			      <button type=\"button\" onclick=\"document.getElementById('id01').style.display='none'\" class=\"cancelbtn\">Cancel</button>
+			      <span class=\"psw\">Forgot <a href=\"#\">password?</a></span>
+			    </div>
+			  </form>
+			</div>
 	    </header>";
 	}
 ?>
