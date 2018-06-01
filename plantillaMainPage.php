909765056f96a702b0253795ec5c54c55f7293b0
Rubén Céspedes Heredia <rubenn569@gmail.com>
2018-06-02 00:14:04 +0200

    Mayor modularización
    Añadido archivo "sidebar.php" como archivo vista para mostrar dos tipos de sidebar, uno sin barra de búsqueda para todas las vistas (a excepción del index) y otro con barra de búsqueda de obras (para index)

--- plantillaMainPage.php
+++ plantillaMainPage.php
@@ -8,6 +8,7 @@ class Vista {
 		include 'head.php';
 		include 'header.php';
 		include 'footer.php';
+		include 'sidebar.php';
 
 		imprimirHead();
 
@@ -16,12 +17,11 @@ class Vista {
 		imprimirHeader();
 
 
-		echo '<section class="index-page page-content">
+		echo '<section class="index-page page-content">';
 
-	        <aside class="menu small-width">
-	            <p>Sidebar de prueba. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Mendrerit id, lorem. Maecenas nec odio e sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
-	        </aside>
-	        <div class="categorias small-width">
+		imprimirSidebar();
+
+		echo '<div class="categorias small-width">
 	            <p class="title">Algunas Obras</p>
 	            <div class="categorias-cards-container">';
 
--- plantillaObra.php
+++ plantillaObra.php
@@ -9,6 +9,7 @@
 			include 'head.php';
 			include 'header.php';
 			include 'footer.php';
+			include 'sidebar.php';
 
 			$glue = "','";
 
@@ -18,13 +19,11 @@
 
 			imprimirHeader();
 
-			echo '<section class="index-page page-content ">
+			echo '<section class="index-page page-content ">';
 
-	        <aside class="menu small-width">
-	            <p>Sidebar de prueba. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Mendrerit id, lorem. Maecenas nec odio e sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,Sidebar de prueba. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. </p>
-	        </aside>
+			imprimirSidebar();
 
-	        <div class="contenido-obra">
+	        echo '<div class="contenido-obra">
 
 	            <!-- CONTENEDOR PARA COMENTARIOS -->
 	            <div id="contenedor" class="contenedor-comentarios">
